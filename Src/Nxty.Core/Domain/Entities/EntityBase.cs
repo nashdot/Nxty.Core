d55b2fdc938eedb558c0d839eb7a96ef4dabce32
Stanislav LESNIKOV <stan@nashdot.com>
2025-04-16 09:50:02 +0200

    Add DomainEvents to EntityBase

--- Src/Nxty.Core/Domain/Entities/EntityBase.cs
+++ Src/Nxty.Core/Domain/Entities/EntityBase.cs
@@ -5,4 +5,18 @@ public abstract class EntityBase<TKey>
     where TKey : IEquatable<TKey>
 {
     public TKey Id { get; init; } = default!;
+
+    private readonly List<IDomainEvent> _domainEvents = [];
+
+    public List<IDomainEvent> DomainEvents => [.. _domainEvents];
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+
+    public void Raise(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
 }
